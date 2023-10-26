667ec96910dcbe7446773d754e64a2b154d9c76b
Jaro Hokkanen <jaro.hokkanen@gmail.com>
2023-10-26 10:01:05 +0300

    Remove the next days first hour

--- st-entsoe.py
+++ st-entsoe.py
@@ -11,18 +11,18 @@ def job():
     client = EntsoePandasClient(api_key="d9268bc7-b025-45e9-8eee-103ed6e84197")
     beginning_of_day = datetime.datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
     start = pd.Timestamp(beginning_of_day, tz='Europe/Helsinki')
-    end = pd.Timestamp(beginning_of_day + datetime.timedelta(days=1), tz='Europe/Helsinki')
+    end = pd.Timestamp(beginning_of_day + datetime.timedelta(days=1, hours=-1), tz='Europe/Helsinki')
     country_code = 'FI'
 
     # Set HeatOff if one of 8 most costly hours of the day and the hourly price is over 4cnt/kWh (VAT excluded)
     prices = client.query_day_ahead_prices('FI', start=start, end=end)
-    if prices.iloc[datetime.datetime.now().hour] > prices.quantile(0.67) and prices.iloc[datetime.datetime.now().hour] > 40 :
-        try :
+    if prices.iloc[datetime.datetime.now().hour] > prices.quantile(0.67) and prices.iloc[datetime.datetime.now().hour] > 40:
+        try:
           requests.post('http://192.168.1.33:8088/HeatOff/trigger')
         except: 
           print("[ERROR] Sending the HeatOff POST request failed at", datetime.datetime.now())
-    else :
-        try :
+    else:
+        try:
           requests.post('http://192.168.1.33:8088/HeatOn/trigger')
         except: 
           print("[ERROR] Sending the HeatOn POST request failed at", datetime.datetime.now())
