9093c3ad0840568cc1bbd05d004c345a2b83e8a0
Jaro Hokkanen <jaro.hokkanen@gmail.com>
2023-11-15 10:13:52 +0200

    Add js branch

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # Smartthings edgebridge electricity price feeder (Entso-E API)
 
-The [st-entsoe.py](st-entsoe.py) python program obtains Finnish electricity prices from the [Entso-E Transparency platform API](https://transparency.entsoe.eu/), and sends an http binary request through the [edgebridge project](https://github.com/toddaustin07/edgebridge) to the [LAN Trigger](https://github.com/toddaustin07/lantrigger) edge driver installed on Smartthings. The [edgebridge.py](https://github.com/toddaustin07/edgebridge/blob/main/edgebridge.py) client is started by [st-entsoe.py](st-entsoe.py) as a subprocess and does not need to be run separately. The `stderr` and `stdout` output streams from [edgebridge.py](https://github.com/toddaustin07/edgebridge/blob/main/edgebridge.py) subprocess are piped through [st-entsoe.py](st-entsoe.py) `stdout` output stream.
+The [st-entsoe.js](st-entsoe.js) nodejs program obtains Finnish electricity prices from the [Entso-E Transparency platform API](https://transparency.entsoe.eu/), and sends an http binary request through the [edgebridge project](https://github.com/toddaustin07/edgebridge) to the [LAN Trigger](https://github.com/toddaustin07/lantrigger) edge driver installed on Smartthings. The [edgebridge.py](https://github.com/toddaustin07/edgebridge/blob/main/edgebridge.py) client is started by [st-entsoe.js](st-entsoe.js) as a subprocess and does not need to be run separately. The `stderr` and `stdout` output streams from [edgebridge.py](https://github.com/toddaustin07/edgebridge/blob/main/edgebridge.py) subprocess are piped through [st-entsoe.js](st-entsoe.js) `stdout` output stream.
 
-NOTE! The device running [st-entsoe.py](st-entsoe.py) should be connected to the same local area network as the Smartthings hub.
+NOTE! The device running [st-entsoe.js](st-entsoe.js) should be connected to the same local area network as the Smartthings hub.
 
 ## Installation
 Clone repo with the [edgebridge](https://github.com/toddaustin07/edgebridge/) as a submodule
@@ -10,26 +10,26 @@ Clone repo with the [edgebridge](https://github.com/toddaustin07/edgebridge/) as
 git clone --recurse-submodules https://github.com/hokkanen/st-entsoe.git
 ```
 
-Install any missing python dependency, usually at least the following:
+Install npm dependencies by running
 ```
-python3 -m pip install entsoe-py
+npm install .
 ```
 
 ## Setup
-The [work directory](workspace) for [st-entsoe.py](st-entsoe.py) (and also [edgebridge.py](https://github.com/toddaustin07/edgebridge/blob/main/edgebridge.py)) should contain an [API key file](workspace/apikey) with the user-specific Entso-E API key, which can be obtained from [Entso-E Transparency platform](https://transparency.entsoe.eu/). The same [work directory](workspace) is also where [edgebridge.py](https://github.com/toddaustin07/edgebridge/blob/main/edgebridge.py) creates its [.registrations](workspace/.registrations) file, after the Smartthings edge drivers are installed and set up with the correct network configuration.
+The [work directory](workspace) for [st-entsoe.js](st-entsoe.js) (and also [edgebridge.py](https://github.com/toddaustin07/edgebridge/blob/main/edgebridge.py)) should contain an [API key file](workspace/apikey) with the user-specific Entso-E API key, which can be obtained from [Entso-E Transparency platform](https://transparency.entsoe.eu/). The same [work directory](workspace) is also where [edgebridge.py](https://github.com/toddaustin07/edgebridge/blob/main/edgebridge.py) creates its [.registrations](workspace/.registrations) file, after the Smartthings edge drivers are installed and set up with the correct network configuration.
 
 ## Running
-Run [st-entsoe.py](st-entsoe.py) in the current terminal instance by
+Run [st-entsoe.js](st-entsoe.js) in the current terminal instance by
 ```
-python3 st-entsoe.py
+node st-entsoe.js
 ```
 In Linux, starting with
 ```
-nohup python3 st-entsoe.py&
+(nohup node st-entsoe.js&)
 ```
 allows the output to be directed to `nohup.out` file and the program to be kept running even if the terminal instance is closed. 
 
-If any conflicting processes are already running (ie, [edgebridge.py](https://github.com/toddaustin07/edgebridge/blob/main/edgebridge.py) or another instance of [st-entsoe.py](st-entsoe.py)), the program does not start and suggest killing these processes first.
+If any conflicting processes are already running (ie, [edgebridge.py](https://github.com/toddaustin07/edgebridge/blob/main/edgebridge.py) or another instance of [st-entsoe.js](st-entsoe.js)), the program does not start and suggest killing these processes first.
 
 ## Edgebridge status monitoring from Smartthings (optional)
 In addition to installing the [LAN Trigger](https://github.com/toddaustin07/lantrigger) edge driver for Smartthings app, it may be convenient to install [EdgeBridge Monitor](https://github.com/toddaustin07/edgebridge#optional-edgebridge-server-monitoring) edge driver as well to monitor the online/offline status of the [edgebridge](https://github.com/toddaustin07/edgebridge/) client.
---
+++ st-entsoe.js
@@ -0,0 +1,149 @@
+const quantile = require( 'compute-quantile' );
+const fetch = require('node-fetch');
+const path = require('path');
+const proc = require('process');
+const ps = require('ps-node');
+const spawn = require('child_process').spawn;
+const schedule = require('node-schedule');
+const { XMLParser, XMLBuilder, XMLValidator}  = require("fast-xml-parser");
+
+// Set debugging settings and prints
+const DEBUG = false;
+
+// Check for any running conflicting processes
+async function check_procs() {
+   
+    // Currently executing script's name and pid
+    const current_filename = path.basename(__filename);
+    const current_pid = proc.pid;
+
+    // Conflicting processes to check for [bin, arg]
+    const conflicts = [
+        ['node', 'st-entsoe.js'],
+        ['python', 'st-entsoe.py'],
+        ['edgebridge', ''],
+        ['python', 'edgebridge.py']
+    ];
+
+    // Iterate over all the processes and collect any conflicting processes
+    const promise = await new Promise((resolve, reject) => {
+        ps.lookup({}, function (err, results) {
+            if (err) {
+                throw new Error(err);
+            }
+            let conflict_procs = [];
+            results.forEach(function (process) {
+                conflicts.forEach(function (conflict) {
+                    if (path.basename(process.command).includes(conflict[0])) {
+                        let args = process.arguments;
+                        if (args.length == 0) args = [''];
+                        args.forEach(function (arg) {
+                            if (path.basename(arg) === conflict[1] && current_pid != process.pid) {
+                                conflict_procs.push(`${process.command} ${arg} (${process.pid})`);
+                            }
+                        });
+                    }
+                });
+            });
+
+            // Check if any conflicting processes found
+            if (conflict_procs.length > 0) {
+                const err_msg = `The following processes prevent ${current_filename} from running!\n` +
+                    ` ${conflict_procs.join('\n ')}\n\nKill these processes (in Linux) by:\n` +
+                    ` kill -9 ${conflict_procs.map((instance) => instance.split(' ').pop().slice(1, -1)).join(' ')}\n`;
+                console.log(err_msg);
+                reject();
+            }else{
+               resolve();     	
+            }
+        });
+    });
+    return promise;
+}
+
+// Launch edgebridge listener to pass messages to Smartthings hub
+async function run_edgebridge() {
+    let eb;
+    if(process.platform === 'win32')
+        eb = spawn(`${__dirname}/edgebridge/edgebridge.exe`, { cwd: `${__dirname}/workspace/`, stdio: 'inherit'});
+    else
+ 		eb = spawn('python3', ['-u', `${__dirname}/edgebridge/edgebridge.py`], { cwd: `${__dirname}/workspace/`, stdio: 'inherit'});
+}
+
+// Query Ensto-E API directly to get the daily spot prices
+async function get_prices() {
+
+    // Get API key from the file "apikey"
+    const fs = require('fs');
+    const api_key = fs.readFileSync('./workspace/apikey', 'utf8').trim();
+
+    // The date is determined from the UTC+1 time because the 24-hour API price period is from 23:00 yesterday to 23:00 today
+    const hour_ahead_utc = new Date(new Date().setTime(new Date().getTime() + (60*60*1000)));
+    let period_start = `${hour_ahead_utc.toISOString().replace(/[-:T.]/g, '').slice(0, 8)}`+`0000`;
+    let period_end = `${hour_ahead_utc.toISOString().replace(/[-:T.]/g, '').slice(0, 8)}`+`2300`;
+
+    // Set compulsory strings for the API call
+    let document_type = `A44`;
+    let process_type = `A01`;
+    let location_id = `10YFI-1--------U`;
+
+    // Send API get request and parse the received xml into json
+    let request = `https://web-api.tp.entsoe.eu/api?securityToken=${api_key}&documentType=${document_type}&processType=${process_type}` +
+        `&in_Domain=${location_id}&out_Domain=${location_id}&periodStart=${period_start}&periodEnd=${period_end}`
+    const response = await fetch(request).catch(error => console.log(error));
+    const json_data = new XMLParser().parse(await response.text());
+
+    // Get price information from the parsed json into the returned prices array
+    let prices = [];
+    json_data.Publication_MarketDocument.TimeSeries.Period.Point.forEach(function (entry) {
+        prices.push(parseFloat(entry['price.amount']));
+    });
+    return prices;
+}
+
+// Control heating by sending a POST request to edgebridge
+async function adjust_heat() {
+
+    // Get daily spot prices
+    prices = await get_prices();
+
+    // Define function for sending a post request to edgebridge
+    const post_trigger = async function (device) {
+        console.log(`[REQUEST] Sending ${device} POST request to edgebridge! (${new Date().toLocaleString('en-GB')})`);
+        const response = await fetch(`http://192.168.1.33:8088/${device}/trigger`, {method: 'POST'}).catch(error => console.log(error));
+    }
+
+    // The index maps to the ceiling of the current UTC hour (0 for 23-00, 1 for 00-01, 2 for 01-02)
+    const index = parseInt(new Date(new Date().setTime(new Date().getTime() + (60*60*1000))).getUTCHours());
+
+    // Send HeatOff request if one of 8 most costly hours of the day and the hourly price is over 4cnt/kWh, else HeatOn
+    if (prices[index] > quantile(prices, 0.67) && prices[index] > 40)
+        await post_trigger("HeatOff");
+    else 
+        await post_trigger("HeatOn");
+
+    // Debugging prints
+    if(DEBUG){
+        console.log(`[DEBUG] price[${index - 1}]: ${prices[index]}, quantile(prices, 0.67): ${quantile(prices, 0.67)} ` +
+            `(${new Date().toISOString().replace(/[T]/g, ' ').slice(0, 19) + " UTC"})`);
+        console.log(prices);
+    }
+}
+
+// Begin here
+(async () => {
+    // Check conflicting processes and exit if found
+    await check_procs().catch(error => process.exit());
+
+    // Start edgebridge as a child process
+    run_edgebridge();
+
+    // Control heating with set schedule
+    if(DEBUG)
+        setInterval(adjust_heat, 3000);
+    else
+        schedule.scheduleJob('0 * * * *', adjust_heat);
+})();
+
+// Keep server running
+process.stdin.resume();
